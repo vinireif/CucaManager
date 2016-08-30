59e79d071c2e063e51433c3615645f86afc56996
vinicius.reif <vinicius.reif@senior.com.br>
2016-08-30 11:23:29 -0300

    Criada Rotas

---
+++ config/route.js
@@ -0,0 +1,10 @@
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateURL: '../views/login.html'
+    })
+    .when('/login', {
+        templateURL: '../views/login.html'
+    })
+    //More Here
+});
