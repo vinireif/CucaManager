afc965f0134053b3905fd445eabb44d39a5cff66
vinicius.reif <vinicius.reif@senior.com.br>
2016-09-05 10:44:37 -0300

    materialize

--- config/app.js
+++ config/app.js
@@ -1,8 +1,1 @@
 var app = angular.module('app', ['ngRoute']);
-app.run(function ($rootScope,$timeout) {
-    $rootScope.$on('$viewContentLoaded', ()=> {
-        $timeout(() => {
-            componentHandler.upgradeAllRegistered();
-        })
-    })
-});
